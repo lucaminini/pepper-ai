b6dcf64418450829f358bdda6d082ed397a80134
MININI <luca.minini@pianadid.local>
2024-04-05 16:31:11 +0200

    base exampoles

---
+++ openaiAPI.py
@@ -0,0 +1,40 @@
+import requests
+import json
+
+OPENAI_API_KEY = "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"
+
+url = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer {}".format(OPENAI_API_KEY)
+}
+
+oldMessages = [
+{
+            "role": "system",
+            "content": "Tu sei un matematico"
+        },
+        {
+            "role": "user",
+            "content": "che numero ricordi?"
+        },
+        {
+            "role": "assistant",
+            "content": "mi ricordo il 456"
+        },
+]
+
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": oldMessages + [
+        {
+            "role": "user",
+            "content": "cosa ti ho chiesto prima? e cosa hai risposto?"
+        }
+    ]
+}
+
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+print(response.json())
